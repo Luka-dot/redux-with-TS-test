707d27f6ffc53cc736274750ebdc7a11412f1658
Luka-dot <79lukask@gmail.com>
2021-02-14 21:35:55 -0500

    added interface for each action.type

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,21 +4,40 @@ interface RepositoriesState {
     data: string[]
 };
 
-interface Action {
-    type: string,
-    payload?: any
+interface SearchRepositoriesAction {
+    type: 'search_repositories'
+};
+
+interface SearchRepositoriesSuccessAction {
+    type: 'search_repositories_success',
+    payload: string[]
+};
+
+interface SearchRepositoriesErrorAction {
+    type: 'search_repositories_error',
+    payload: string
 }
 
+// interface Action {
+//     type: string,
+//     payload?: any
+// }
 // (state, action):RepositoryState in this case ensuring what is returned from any case is right format.
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+//const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+
+
+const reducer = (
+    state: RepositoriesState, 
+    action: SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction 
+    ): RepositoriesState => {
     switch (action.type) {
         case 'search_repositories':
             return { loading: true, error: null, data: []};
 
-        case 'earch_repositories_success':
+        case 'search_repositories_success':
             return { loading: false, error: null, data: action.payload };
 
-        case 'earch_repositories_error':    
+        case 'search_repositories_error':    
             return { loading: false, error: action.payload, data: [] };
 
         default:
